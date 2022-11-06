225c1d5375e0e9582044f20561d2c8c8959796c6
shakedguy <shakedguy94@gmail.com>
2022-11-06 13:22:56 +0200

    update

--- src/Router.jsx
+++ src/Router.jsx
@@ -6,17 +6,18 @@ import Error from './components/errors/Error';
 import App from './App';
 
 const urls = [
-	'/about/',
-	'/info/calculator/',
-	'/info/child-support/',
-	'/info/consult/',
-	'/404/',
-	'/info/financial-help/',
-	'/home/',
-	'/info/',
-	'/info/insurance/',
-	'/registration/',
-	'/info/tools/',
+	'/',
+	'/about',
+	'/info/calculator',
+	'/info/child-support',
+	'/info/consult',
+	'/404',
+	'/info/financial-help',
+	'/home',
+	'/info',
+	'/info/insurance',
+	'/registration',
+	'/info/tools',
 ];
 
 const Router = () => {
@@ -30,18 +31,25 @@ const Router = () => {
 
 	return (
 		<Routes>
-			<Route
+			{/* <Route
 				path='/'
 				element={<App />}
-			/>
-			<Route
+			/> */}
+			{urls.map((url, index) => (
+				<Route
+					key={index}
+					path={`${url}`}
+					element={<App />}
+				/>
+			))}
+			{/* <Route
 				path='/:pageName'
 				element={<App />}
 			/>
 			<Route
 				path='/:pageName/:slug'
 				element={<App />}
-			/>
+			/> */}
 			{/* {slugs.map((slug, index) => (
 				<Route
 					key={index}
