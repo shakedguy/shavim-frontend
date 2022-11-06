d58cb3752a31db69549556933b2cc89aba4ff86a
shakedguy <shakedguy94@gmail.com>
2022-11-06 13:53:41 +0200

    update

--- src/index.js
+++ src/index.js
@@ -9,18 +9,16 @@ import Router from './Router';
 import reportWebVitals from './reportWebVitals';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { CookiesProvider } from 'react-cookie';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
 const queryClient = new QueryClient();
 
-//guy
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<HashRouter>
 			<CookiesProvider>
 				<QueryClientProvider client={queryClient}>
 					<Provider store={store}>
@@ -28,7 +26,7 @@ root.render(
 					</Provider>
 				</QueryClientProvider>
 			</CookiesProvider>
-		</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>
 );
 
