e553efddc0a06c39b9cab67da7c78b0412d26517
shakedguy <shakedguy94@gmail.com>
2022-11-06 13:40:01 +0200

    update

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@ import store from './store';
 
 const queryClient = new QueryClient();
 
+//guy
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
